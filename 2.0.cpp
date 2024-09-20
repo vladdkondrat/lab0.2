f47000ef8328200e39e9e757de9f7db083cc7584
kondratvlad <vladkondrat87@gmail.com>
2024-09-20 10:24:36 +0300

    first master commit

---
+++ 2.0.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath> // Для математичних функцій, наприклад, sin і cos
+
+using namespace std;
+
+int main() {
+    // Оголошення змінних
+    double alpha, beta;
+    double z1;
+    // double z2; // Закоментовано
+
+    // Введення значень alpha та beta
+    cout << "alpha =: ";
+    cin >> alpha;
+    cout << " beta =: ";
+    cin >> beta;
+
+    // Обчислення z1
+    z1 = (sin(alpha) + cos(2 * beta - alpha)) / (cos(alpha) - sin(2 * beta - alpha));
+
+    // Обчислення z2
+    // z2 = (1 + sin(2 * beta)) / cos(2 * beta); // Закоментовано
+
+    // Виведення результатів
+    cout << " z1=: " << z1 << endl;
+    // cout << " z2=: " << z2 << endl; // Закоментовано
+
+    return 0;
+}
